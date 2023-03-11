d22dac2b45347137293d4f2d7e67520e2f75f38a
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-11 05:36:06 +0330

    Function: sandwichTrick

--- enchantments.js
+++ enchantments.js
@@ -30,3 +30,16 @@ function middleTwo(deck) {
 }
 
 // console.log(middleTwo(deck));
+
+///---///
+
+// const deck = [1, 2, 3, 5, 6, 10];
+
+function sandwichTrick(deck) {
+  const mid = deck.length / 2;
+  deck.splice(mid, 0, deck.pop());
+  deck.splice(mid, 0, deck.shift());
+  return deck;
+}
+
+// console.log(sandwichTrick(deck));
