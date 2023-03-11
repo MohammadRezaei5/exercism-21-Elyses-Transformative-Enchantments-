498208204c6faffb92db94959f04a03f7b43c7f2
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-11 05:35:04 +0330

    Function: threeOfEachThree

--- enchantments.js
+++ enchantments.js
@@ -6,3 +6,16 @@ function seeingDouble(deck) {
   return deck.map((card) => card * 2);
 }
 // console.log(seeingDouble(deck));
+
+///---///
+
+// const deck = [1, 3, 9, 3, 7];
+
+function threeOfEachThree(deck) {
+  return deck.reduce((newArray, card) => {
+    card === 3 ? newArray.push(card, card, card) : newArray.push(card);
+    return newArray;
+  }, []);
+}
+
+// console.log(threeOfEachThree(deck));
